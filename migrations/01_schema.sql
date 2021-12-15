9c90d3cf7cede45d42bdbaa2745d3994bfb8f484
jackiestchen <jackiestchen@gmail.com>
2021-12-14 22:34:07 -0700

    Initialize database schema

---
+++ migrations/01_schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email TEXT,
+  password TEXT
+)
+
+CREATE TABLE properties (
+  id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE
+);
