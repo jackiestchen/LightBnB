5360b5302734d74a33650d3b5b7172ab1314da42
jackiestchen <jackiestchen@gmail.com>
2021-12-19 23:11:50 -0700

    Change addUser function to use database

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -28,7 +28,7 @@ const getUserWithEmail = function (email) {
   return pool
     .query(queryString, values)
     .then((res) => {
-      if (res.rows.length < 1){
+      if (res.rows.length < 1) {
         return null;
       }
       return res.rows[0];
@@ -72,10 +72,25 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  // const userId = Object.keys(users).length + 1;
+  // user.id = userId;
+  // users[userId] = user;
+  // return Promise.resolve(user);
+  const queryString = `
+  INSERT INTO users (name, email, password)
+  VALUES ($1, $2, $3)
+  RETURNING *;
+  `;
+  const values = [user.name, user.email, user.password];
+
+  return pool
+    .query(queryString, values)
+    .then((res) => {
+      return res.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 exports.addUser = addUser;
 
